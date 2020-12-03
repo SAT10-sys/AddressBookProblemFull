380c2431af135dcb498bd496256f4ee110015620
Satantra Tewari <satantra1508@gmail.com>
2020-12-03 23:34:27 +0530

    [satantra] added method to add contact

--- AddressBookProblemFull/AddressBookMain.cs
+++ AddressBookProblemFull/AddressBookMain.cs
@@ -7,5 +7,14 @@ namespace AddressBookProblemFull
     public class AddressBookMain
     {
         public List<Contacts> contactList;
+        public AddressBookMain()
+        {
+            contactList = new List<Contacts>();
+        }
+        public void AddContact(string firstName, string lastName, string address, string city, string state, string zipCode, string phoneNumber, string emailID)
+        {
+            Contacts contacts = new Contacts(firstName, lastName, address, city, state, zipCode, phoneNumber, emailID);
+            contactList.Add(contacts);
+        }
     }
 }
