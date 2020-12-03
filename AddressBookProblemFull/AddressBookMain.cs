d1bbd9eeb504ba43d1d46bc9854f3b4b4e1ff289
Satantra Tewari <satantra1508@gmail.com>
2020-12-03 23:44:33 +0530

    [satantra] added methods to edit contact and check for firstname

--- AddressBookProblemFull/AddressBookMain.cs
+++ AddressBookProblemFull/AddressBookMain.cs
@@ -16,5 +16,30 @@ namespace AddressBookProblemFull
             Contacts contacts = new Contacts(firstName, lastName, address, city, state, zipCode, phoneNumber, emailID);
             contactList.Add(contacts);
         }
+        public void EditContact(string firstName, string lastName, string address, string city, string state, string zipCode, string phoneNumber, string emailID)
+        {
+            foreach(Contacts c in contactList)
+            {
+                if(c.firstName.Equals(firstName))
+                {
+                    c.lastName = lastName;
+                    c.address = address;
+                    c.city = city;
+                    c.state = state;
+                    c.zipCode = zipCode;
+                    c.phoneNumber = phoneNumber;
+                    c.emailID = emailID;
+                }
+            }
+        }
+        public bool CheckFirstName(string firstName)
+        {
+            foreach(Contacts c in contactList)
+            {
+                if (c.firstName.Equals(firstName))
+                    return true;                   
+            }
+            return false;
+        }
     }
 }
