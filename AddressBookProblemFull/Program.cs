187677dfa1a37a260a0e776fa01078326623c9f4
Satantra Tewari <satantra1508@gmail.com>
2020-12-03 23:38:18 +0530

    [satantra] contact added

--- AddressBookProblemFull/Program.cs
+++ AddressBookProblemFull/Program.cs
@@ -6,7 +6,13 @@ namespace AddressBookProblemFull
     {
         static void Main(string[] args)
         {
+            AddressBookMain addressBookMain = new AddressBookMain();
             Console.WriteLine("Welcome to Address Book Problem ");
+            Console.WriteLine("Enter the following details seperated by ,");
+            string[] details;
+            details = Console.ReadLine().Split(",");
+            addressBookMain.AddContact(details[0], details[1], details[2], details[3], details[4], details[5], details[6], details[7]);
+            Console.WriteLine("Contact added");
         }
     }
 }
