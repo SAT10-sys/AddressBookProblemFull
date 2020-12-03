fbbe856536e218008905d500bfd793cbfb5882a9
Satantra Tewari <satantra1508@gmail.com>
2020-12-03 23:27:58 +0530

    [satantra] contacts created

---
+++ AddressBookProblemFull/AddressBookMain.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookProblemFull
+{
+    public class AddressBookMain
+    {
+        public List<Contacts> contactList;
+    }
+}
---
+++ AddressBookProblemFull/Contacts.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookProblemFull
+{
+    public class Contacts
+    {
+        public string firstName;
+        public string lastName;
+        public string address;
+        public string city;
+        public string state;
+        public string zipCode;
+        public string phoneNumber;
+        public string emailID;
+        public Contacts()
+        {
+            firstName = "";
+            lastName = "";
+            address = "";
+            city = "";
+            state = "";
+            zipCode = "";
+            phoneNumber = "";
+            emailID = "";
+        }
+        public Contacts(string firstName, string lastName, string address, string city, string state, string zipCode, string phoneNumber, string emailID)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zipCode = zipCode;
+            this.phoneNumber = phoneNumber;
+            this.emailID = emailID;
+        }
+    }
+}
