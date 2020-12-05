7feeba8c02ff97d1edbebbde0df1c016add5dd82
Satantra Tewari <satantra1508@gmail.com>
2020-12-05 13:50:53 +0530

    [satantra] added multiple address books

---
+++ AddressBookProblemFull/MultipleAddressBooks.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookProblemFull
+{
+    public class MultipleAddressBooks
+    {
+        Dictionary<string, AddressBookMain> addressBookCollection = new Dictionary<string, AddressBookMain>();
+        public MultipleAddressBooks()
+        {
+            addressBookCollection = new Dictionary<string, AddressBookMain>();
+        }
+        public void AddAddressBook(string name)
+        {
+            AddressBookMain addressBookMain = new AddressBookMain();
+            addressBookCollection.Add(name, addressBookMain);
+        }
+        public AddressBookMain GetAddressBook(string name)
+        {
+            if (addressBookCollection.ContainsKey(name))
+                return addressBookCollection[name];
+            return null;
+        }
+    }
+}
--- AddressBookProblemFull/Program.cs
+++ AddressBookProblemFull/Program.cs
@@ -6,59 +6,87 @@ namespace AddressBookProblemFull
     {
         static void Main(string[] args)
         {
-            AddressBookMain addressBookMain = new AddressBookMain();
-            Console.WriteLine("Welcome to Address Book Problem ");
-            Console.WriteLine("Enter choices");
-            Console.WriteLine("1.ADD CONTACT\n2.EDIT CONTACT\n3.REMOVE CONTACT");
-            int choice = Convert.ToInt32(Console.ReadLine());
-            switch(choice)
+            MultipleAddressBooks multipleAddressBooks = new MultipleAddressBooks();
+            AddressBookMain addressBookMain;
+            addressBookMain = null;
+            while (true)
             {
-                case 1:
-                    RegStart:
-                    Console.WriteLine("Enter the following details seperated by ,");
-                    string[] details;
-                    details = Console.ReadLine().Split(",");
-                    addressBookMain.AddContact(details[0], details[1], details[2], details[3], details[4], details[5], details[6], details[7]);
-                    Console.WriteLine("Contact added");
-                    Console.WriteLine("Add More Contacts(Yes/No");
-                    string ans = Console.ReadLine().ToLower();
-                    if (ans == "yes")                    
-                        goto RegStart;                          
-                    else if (ans == "no")
+                bool flag = true;
+                Console.WriteLine("Name of Address Book");
+                string name = Console.ReadLine();
+                Console.WriteLine("1.ADD ADDRESSBOOK\n2.OPEN ADDRESSBOOK");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                if (choice == 1)
+                {
+                    multipleAddressBooks.AddAddressBook(name);
+                    addressBookMain = multipleAddressBooks.GetAddressBook(name);
+                }
+                else if (choice == 2)
+                {
+                    addressBookMain = multipleAddressBooks.GetAddressBook(name);
+                    if (addressBookMain == null)
+                        Console.WriteLine("No such address book");
+                }
+                else
+                { Console.WriteLine("Incorrect choice"); break; }
+                while (flag)
+                {
+                    if (addressBookMain == null)
                         break;
-                    else
-                        Console.WriteLine("Invalid choice");
-                    break;
-                case 2:
-                    Console.WriteLine("Enter First Name of the Contact to be edited");
-                    string firstName = Console.ReadLine();
-                    bool result = addressBookMain.CheckFirstName(firstName);
-                    if (result == true)
+                    Console.WriteLine("Enter choices");
+                    Console.WriteLine("1.ADD CONTACT\n2.EDIT CONTACT\n3.REMOVE CONTACT");
+                    int choiceOfOperation = Convert.ToInt32(Console.ReadLine());
+                    switch (choiceOfOperation)
                     {
-                        Console.WriteLine("Enter the edited details seperated by ,");
-                        string[] newDetails = Console.ReadLine().Split(",");
-                        addressBookMain.EditContact(firstName, newDetails[0], newDetails[1], newDetails[2], newDetails[3], newDetails[4], newDetails[5], newDetails[6]);
-                        Console.WriteLine("Contact edited successfully");
+                        case 1:
+                        RegStart:
+                            Console.WriteLine("Enter the following details seperated by ,");
+                            string[] details;
+                            details = Console.ReadLine().Split(",");
+                            addressBookMain.AddContact(details[0], details[1], details[2], details[3], details[4], details[5], details[6], details[7]);
+                            Console.WriteLine("Contact added");
+                            Console.WriteLine("Add More Contacts(Yes/No");
+                            string ans = Console.ReadLine().ToLower();
+                            if (ans == "yes")
+                                goto RegStart;
+                            else if (ans == "no")
+                                break;
+                            else
+                                Console.WriteLine("Invalid choice");
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter First Name of the Contact to be edited");
+                            string firstName = Console.ReadLine();
+                            bool result = addressBookMain.CheckFirstName(firstName);
+                            if (result == true)
+                            {
+                                Console.WriteLine("Enter the edited details seperated by ,");
+                                string[] newDetails = Console.ReadLine().Split(",");
+                                addressBookMain.EditContact(firstName, newDetails[0], newDetails[1], newDetails[2], newDetails[3], newDetails[4], newDetails[5], newDetails[6]);
+                                Console.WriteLine("Contact edited successfully");
+                            }
+                            else
+                                Console.WriteLine("No contacts found");
+                            break;
+                        case 3:
+                            Console.WriteLine("Enter First Name of the Contact to be deleted");
+                            string firstName1 = Console.ReadLine();
+                            bool result1 = addressBookMain.CheckFirstName(firstName1);
+                            if (result1 == true)
+                            {
+                                addressBookMain.RemoveContact(firstName1);
+                                Console.WriteLine("Contact deleted successfully");
+                            }
+                            else
+                                Console.WriteLine("No contacts found");
+                            break;
+                        default:
+                            flag = false;
+                            break;
                     }
-                    else
-                        Console.WriteLine("No contacts found");
-                    break;
-                case 3:
-                    Console.WriteLine("Enter First Name of the Contact to be deleted");
-                    string firstName1 = Console.ReadLine();
-                    bool result1 = addressBookMain.CheckFirstName(firstName1);
-                    if (result1 == true)
-                    {
-                        addressBookMain.RemoveContact(firstName1);                     
-                        Console.WriteLine("Contact deleted successfully");
-                    }
-                    else
-                        Console.WriteLine("No contacts found");
-                    break;
-                default:
-                    Console.WriteLine("Invalid choice");
-                    break;
-            }                       
+                }
+            }
         }
     }
 }
+    
